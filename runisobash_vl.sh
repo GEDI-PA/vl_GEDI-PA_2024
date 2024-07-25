1489768ff938580285b652e286282e3e8d29c5f4
leitoldv <vleitold@umd.edu>
2024-07-25 09:48:06 -0700

    make output dir

--- runisobash_vl.sh
+++ runisobash_vl.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 basedir=$( cd "$(dirname "$0")" ; pwd -P )
 
+mkdir -p output
+
 # Initialize variables
 country=""
 
@@ -12,6 +14,8 @@ country=""
 #	esac
 #done
 country=${1}
+#output_filename=${2}
 
 #Rscript ${basedir}/global_process_part1_2024_MAAP_step123.R $country
 conda run --no-capture-output --name gedi_pa_env Rscript ${basedir}/global_process_part1_2024_MAAP_step123.R $country
+#--output_file output/${output_filename}
