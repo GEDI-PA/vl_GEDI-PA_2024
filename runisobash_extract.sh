ba6bd4ad1a78015251f9515704137b820ccde535
[abarenblitt] <acbarenblitt@gmail.com>
2024-09-09 16:44:07 -0700

    run gedi extract code

---
+++ runisobash_extract.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+basedir=$( cd "$(dirname "$0")" ; pwd -P )
+
+mkdir -p output
+
+# Initialize variables
+country=""
+
+# Parse command-line options
+#while getopts c: flag
+#do
+#	case "${flag}" in
+#		c) country=${OPTARG};;
+#	esac
+#done
+country=${1}
+#output_filename=${2}
+
+#Rscript ${basedir}/global_process_part1_2024_MAAP_step123.R $country
+cd ${basedir}
+conda run --no-capture-output --name gedi_pa_env Rscript global_process_part3_2024_MAAP_step5.r $country ${basedir}/output
+
