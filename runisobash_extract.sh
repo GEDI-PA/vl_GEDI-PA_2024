ceca6d320f81d04b48113d16c51df540a8ee50cf
[abarenblitt] <acbarenblitt@gmail.com>
2024-10-29 06:37:32 -0700

    tried adding back in cd basedir

--- runisobash_extract.sh
+++ runisobash_extract.sh
@@ -6,5 +6,6 @@ mkdir -p output
 country=${1}
 
 #Rscript ${basedir}/global_process_part1_2024_MAAP_step123.R $country
+cd ${basedir}
 conda run --no-capture-output --name gedi_pa_env Rscript ${basedir}/global_process_part3_2024_MAAP_step5.r $country output
 
