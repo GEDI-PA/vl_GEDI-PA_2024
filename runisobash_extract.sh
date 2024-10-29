0020191d7f6f5b5043c3d85710d14c14a495935e
[abarenblitt] <acbarenblitt@gmail.com>
2024-10-29 10:24:22 -0700

    corrected current path and output path

--- runisobash_extract.sh
+++ runisobash_extract.sh
@@ -2,10 +2,12 @@
 basedir=$( cd "$(dirname "$0")" ; pwd -P )
 
 mkdir -p output
+outdir=${PWD}/ouput
 
 country=${1}
 
-#Rscript ${basedir}/global_process_part1_2024_MAAP_step123.R $country
-cd ${basedir}
-conda run --no-capture-output --name gedi_pa_env Rscript ${basedir}/global_process_part3_2024_MAAP_step5.r $country output
 
+
+#Rscript ${basedir}/global_process_part1_2024_MAAP_step123.R $country $outdir
+cd ${basedir}
+conda run --no-capture-output --name gedi_pa_env Rscript global_process_part3_2024_MAAP_step5.r $country $outdir
