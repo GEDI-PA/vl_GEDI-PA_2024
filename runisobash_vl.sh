7fa41cf0228c4e6945ff12b50a302de1958ff896
[abarenblitt] <acbarenblitt@gmail.com>
2024-11-20 10:57:54 -0800

    Corrected output connection

--- runisobash_vl.sh
+++ runisobash_vl.sh
@@ -2,21 +2,12 @@
 basedir=$( cd "$(dirname "$0")" ; pwd -P )
 
 mkdir -p output
+outdir=${PWD}/output
 
-# Initialize variables
-country=""
-
-# Parse command-line options
-#while getopts c: flag
-#do
-#	case "${flag}" in
-#		c) country=${OPTARG};;
-#	esac
-#done
 country=${1}
-#output_filename=${2}
 
-#Rscript ${basedir}/global_process_part1_2024_MAAP_step123.R $country
+
+
+#Rscript ${basedir}/global_process_part1_2024_MAAP_step123.R $country $outdir
 cd ${basedir}
-conda run --no-capture-output --name gedi_pa_env Rscript global_process_part1_2024_MAAP_step123.R $country ${basedir}/output
-
+conda run --no-capture-output --name gedi_pa_env Rscript global_process_part3_2024_MAAP_step123.r $country $outdir
