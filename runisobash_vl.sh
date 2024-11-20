42e86624d8d229a3024fdcc8d603d9d42a63b75e
[abarenblitt] <acbarenblitt@gmail.com>
2024-11-20 13:43:50 -0800

    Fixed naming error

--- runisobash_vl.sh
+++ runisobash_vl.sh
@@ -10,4 +10,4 @@ country=${1}
 
 #Rscript ${basedir}/global_process_part1_2024_MAAP_step123.R $country $outdir
 cd ${basedir}
-conda run --no-capture-output --name gedi_pa_env Rscript global_process_part3_2024_MAAP_step123.r $country $outdir
+conda run --no-capture-output --name gedi_pa_env Rscript global_process_part1_2024_MAAP_step123.r $country $outdir
