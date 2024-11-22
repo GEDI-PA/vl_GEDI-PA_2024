483d1453ddabb82cf0a9147b5a3f16eb6ed31fdc
[abarenblitt] <acbarenblitt@gmail.com>
2024-11-22 11:00:22 -0800

    Matching script .sh file

---
+++ runisobash_matching.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+basedir=$( cd "$(dirname "$0")" ; pwd -P )
+
+mkdir -p output
+outdir=${PWD}/output
+
+country=${1}
+
+
+
+#Rscript ${basedir}/global_process_part1_2024_MAAP_step123.R $country $outdir
+cd ${basedir}
+conda run --no-capture-output --name gedi_pa_env Rscript global_process_part2_2024_MAAP_step4.r $country $outdir
