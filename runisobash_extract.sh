6e4afcce97642828dec7b2ae0b060466c7225964
[abarenblitt] <acbarenblitt@gmail.com>
2025-01-14 10:47:09 -0800

    Added arg for range of PAs

--- runisobash_extract.sh
+++ runisobash_extract.sh
@@ -6,8 +6,9 @@ outdir=${PWD}/output
 
 country=${1}
 
+range=${3}
 
 
 #Rscript ${basedir}/global_process_part1_2024_MAAP_step123.R $country $outdir
 cd ${basedir}
-conda run --no-capture-output --name gedi_pa_env Rscript global_process_part3_2024_MAAP_step5.r $country $outdir
+conda run --no-capture-output --name gedi_pa_env Rscript global_process_part3_2024_MAAP_step5.r $country $outdir $range 
